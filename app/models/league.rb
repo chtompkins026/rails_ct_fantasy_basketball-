a4016fc5a4e3d46a74bd2a6a0d39536130f2807d
Chris Tompkins <chtompkins026@gmail.com>
2019-01-30 17:06:39 -0500

    Added new validations and custom messages to league and team model

--- app/models/league.rb
+++ app/models/league.rb
@@ -2,6 +2,5 @@ class League < ActiveRecord::Base
   has_many :teams
   has_many :users, through: :team
   validates_presence_of :name
-  validates_uniqueness_of :name
-
+  validates_uniqueness_of :name, message: "- league name already exists!"
 end
