b2a280efcc484bed69c52119146feb1962e3f871
Chris Tompkins <chtompkins026@gmail.com>
2019-01-30 17:13:06 -0500

    Update to custom message in the team model validations

--- app/models/team.rb
+++ app/models/team.rb
@@ -4,6 +4,6 @@ class Team < ActiveRecord::Base
   has_many :players
 
   validates_presence_of :name, :total_points, :user_id, :league_id
-  validates_uniqueness_of :name, message: "- this team name is already taken"
-   validates_uniqueness_of :league_id, message: "- you already have a team in this league!"
+  validates_uniqueness_of :name, message: "Error - this team name is already taken"
+   validates_uniqueness_of :league_id, message: "Error - you already have a team in this league!"
 end
