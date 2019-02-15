6f2835d2b6c6aeca637fd49404fca6c397c88714
Chris Tompkins <chtompkins026@gmail.com>
2019-02-15 17:31:32 -0500

    Users can now successfully delete teams. This will delete the team from the database, but also update the players on the team so that they don't have a user_id and can now be picked up by another user

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -43,15 +43,30 @@ class TeamsController < ApplicationController
   end
 
   def destroy
-    @team = Team.find(params[:id])
-    user = @team.user_id 
-    @team.update(team_params)
+    team = Team.find(params[:id])
+    user_id = team.user_id
+    team.players.each do |p|
+      player_league_help(p, team, user_id)
+    end
+
+    team.destroy
+    flash[:success] = "Successfully deleted team!"
+    redirect_to user_path(user_id)
   end
 
   private
 
   def team_params
     params.require(:team).permit(:name, :league_id, :user_id)
+  end
+
+  def player_league_help(player, team, user_id)
+    if player.player_leagues.pluck(:league_id).include?(team.league.id) && user_id == current_user.id
+      player_league = PlayerLeague.find_by({league_id: team.league.id, player_id: player.id})
+      player_league.destroy
+      player.update(team_id: nil)
+      player.save
+    end
   end
 
 end
